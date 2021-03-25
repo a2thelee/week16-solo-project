5f234605321295724c7d0f3dacc2f5f82096be20
Andrew Lee <andrewyujaylee@gmail.com>
2021-03-25 15:53:53 -0400

    onto phase 4

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,24 +1,40 @@
 const router = require("express").Router();
-
-
-
-
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    },
-  })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
+const { restoreUser } = require("../../utils/auth.js")
+const { requireAuth } = require("../../utils/auth.js");
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-})
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // GET /api/require-auth
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: 'Demo-lition'
+//     },
+//   })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// })
 
 
 module.exports = router;

526b0178bab2f45c3de7e98a156b9f00d2d5176a
Andrew Lee <andrewyujaylee@gmail.com>
2021-03-26 16:32:35 -0400

    finished basic phases

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -61,6 +61,18 @@ export const signup = (user) => async (dispatch) => {
   return response;
 };
 
+//user logout
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+
+
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {

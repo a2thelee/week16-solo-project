ee01812d3b6f2069fe3a44124c792f8c77a743bb
Andrew Lee <andrewyujaylee@gmail.com>
2021-03-31 07:03:33 -0400

    started backend search route for later

---
+++ backend/routes/api/search.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { Venue } = require('../../db/models');
+
+const router = express.Router();
+
+
+
+module.exports = router;
